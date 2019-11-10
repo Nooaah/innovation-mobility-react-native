1ddd8c5f74542abe320057d374500ca2d43128c8
Nooaah <noah.chtl@gmail.com>
2019-11-10 12:43:01 +0100

    Add page shop with navigation to this page

--- Components/Shop.js
+++ Components/Shop.js
@@ -9,20 +9,27 @@ class Shop extends React.Component {
 
     
     render() {
+        {/* alignItems : mettre au centre en horizontal -- justifyContent : mettre au centre en vertical */}
         var user = this.props.navigation.state.params.user
         user = JSON.parse(user);
         //console.log(user)
         //Faire l'appel API pour récupérer dans la base ici
         const points = 15
         return (
-            <View style={{ flex : 1, justifyContent : 'center', alignItems : 'center' }}>
+            <View style={{ flex : 1 }}>
 
-                {{ /* Create card of shops here */ }}
-            
+                <View style={{ flex : 1 }}>
+                    <Animatable.Text 
+                    style={{ textAlign : 'center', marginTop : 20 }}
+                    animation="bounceIn"
+                    >
+                    Voici les réductions du moment { user.firstname.charAt(0).toUpperCase() + user.firstname.slice(1) } !
+                    </Animatable.Text>
+                </View>
+
+                { /* Mettre ici ma liste d'items TODO : Rajouter à l'API node les reductions */ }
 
                 <FooterMenu nav={this.props}/>
-
-
             </View>
         )
     }
